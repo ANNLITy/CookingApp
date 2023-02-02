fe3b537457fad1536a4d4aa35ba0f642211313f5
Al040 <klpk44@yandex.ru>
2023-02-02 14:43:15 +0300

    dz

--- src/main/java/com/example/foodapp/controllers/FirstController.java
+++ src/main/java/com/example/foodapp/controllers/FirstController.java
@@ -3,3 +3,18 @@ package com.example.foodapp.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class FirstController {
+
+
+    @GetMapping
+    public String start(){
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        return "Имя ученика: Анна Литяева\nПроект:FoodApp\nДата создания:02.02.2023\nОписание:приложение для сайта рецептов";
+
+    }
+}
